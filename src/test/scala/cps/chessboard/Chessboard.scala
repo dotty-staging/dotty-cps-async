dde627372f5501a074505281edbb7bcd008d9fbc
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-08-17 13:43:35 +0200

    Update to new extension syntax

--- src/test/scala/cps/chessboard/Chessboard.scala
+++ src/test/scala/cps/chessboard/Chessboard.scala
@@ -11,38 +11,38 @@ case class Chessman(figure: Figure, color: Color)
 opaque type Chessboard = Map[(Int,Int),Chessman]
 
 object Chessboard:
-   def empty = Map.empty
-   val size = 8
+  def empty = Map.empty
+  val size = 8
 
-   extension chessboardOps on (board: Chessboard):
+  extension (board: Chessboard):
 
-       def put(x:Int, y:Int, chessman: Chessman): Option[Chessboard] = 
-         require(board.isInside(x,y))
-         val xy = (x,y)
-         board.get(xy) match 
-           case Some(c) => None
-           case None => Some(board.updated(xy,chessman))
-       
-      
-       def check(x:Int, y:Int): Option[Chessman] =
-         board.get((x,y))
+    def put(x:Int, y:Int, chessman: Chessman): Option[Chessboard] =
+      require(board.isInside(x,y))
+      val xy = (x,y)
+      board.get(xy) match
+        case Some(c) => None
+        case None => Some(board.updated(xy,chessman))
 
-       def remove(x:Int, y:Int): Chessboard =
-         board.removed((x,y))
 
-       def isInside(x:Int, y:Int): Boolean =
-         x >= 0 && x < Chessboard.size &&
-         y >= 0 && y < Chessboard.size
+    def check(x:Int, y:Int): Option[Chessman] =
+      board.get((x,y))
 
-       def color(x:Int, y:Int): Color =
-         if ((x+y) % 2 == 0)
-           Color.Black
-         else
-           Color.White
+    def remove(x:Int, y:Int): Chessboard =
+      board.removed((x,y))
 
-       def isFull: Boolean =
-         (0 until size).forall(x =>
-           (0 until size).forall(y =>
-             check(x,y).isDefined
-                              ) )
-              
+    def isInside(x:Int, y:Int): Boolean =
+      x >= 0 && x < Chessboard.size &&
+      y >= 0 && y < Chessboard.size
+
+    def color(x:Int, y:Int): Color =
+      if ((x+y) % 2 == 0)
+        Color.Black
+      else
+        Color.White
+
+    def isFull: Boolean =
+      (0 until size).forall(x =>
+        (0 until size).forall(y =>
+          check(x,y).isDefined
+                          ) )
+  end extension
